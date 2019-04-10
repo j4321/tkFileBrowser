53345419576d8da8d93fa0063c46cf0c5b572247
Juliette Monsel <j_4321@protonmail.com>
2019-04-10 22:44:54 +0200

    Fix click on root button in path bar

--- tkfilebrowser/filebrowser.py
+++ tkfilebrowser/filebrowser.py
@@ -994,7 +994,7 @@ class FileBrowser(tk.Toplevel):
         else:
             p = "/"
             b = PathButton(self.path_bar, self.path_var, p, image=self.im_drive,
-                           command=lambda path=p: self.display_folder(p, update_bar=False))
+                           command=lambda path=p: self.display_folder(path, update_bar=False))
         self.path_bar_buttons.append(b)
         b.grid(row=0, column=1, sticky="ns")
         for i, folder in enumerate(folders):
